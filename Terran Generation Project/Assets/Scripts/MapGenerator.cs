2919d6382ad2b8b22782dcad1efff199a172b2c3
JayWilling <jwilling123@gmail.com>
2021-06-16 16:25:32 +1000

    Added colour regions
    Colour regions added, texture generation and display split to accommodate noise map and colour map display

--- Terran Generation Project/Assets/Scripts/MapGenerator.cs
+++ Terran Generation Project/Assets/Scripts/MapGenerator.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class MapGenerator : MonoBehaviour
 {
 
+    public enum DrawMode {NoiseMap, ColourMap};
+    public DrawMode drawMode;
+
     public int mapWidth;
     public int mapHeight;
     public float noiseScale;
@@ -19,13 +22,29 @@ public class MapGenerator : MonoBehaviour
 
     public bool autoUpdate;
 
+    public TerrainType[] regions;
+
     public void GenerateMap() {
         float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, seed, octaves, persistence, lacunarity, offset);
+        Color[] colourMap = new Color[mapWidth * mapHeight];
 
-        // TODO: Add in lacunarity and persistence
+        for (int y = 0; y < mapHeight; y++) {
+            for (int x = 0; x < mapWidth; x++) {
+                for (int i = 0; i < regions.Length; i++) {
+                    if (noiseMap[x,y] <= regions[i].height) {
+                        colourMap[y * mapWidth + x] = regions[i].colour;
+                        break;
+                    }
+                }
+            }
+        }
 
         MapDisplay display = FindObjectOfType<MapDisplay> ();
-        display.DrawNoiseMap(noiseMap);
+        if (drawMode == DrawMode.NoiseMap) {
+            display.DrawTexture(TextureGenerator.TextureFromHeightMap(noiseMap));
+        } else if (drawMode == DrawMode.ColourMap) {
+            display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeight));
+        }
     }
 
     private void OnValidate() {
